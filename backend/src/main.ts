2fed5c0b86b9314dead4dabc12a26ae195d962b0
Mohd Shahalam <officialshahalam855@gmail.com>
2025-09-08 13:51:57 +0530

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,11 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://e-comerce-assignment.vercel.app",
-      "https://e-comerce-assignment.onrender.com"
-    ],
+    origin: ["*"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
   })
